ffc999ba08ce530f55dbc3ed76316f2c1ab16686
Pete Gadomski <pete.gadomski@gmail.com>
2026-03-11 13:48:52 -0600

    fix: disable react strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -7,11 +6,9 @@ import App from "./App";
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <QueryClientProvider client={client}>
-        <App />
-      </QueryClientProvider>
-    </ChakraProvider>
-  </StrictMode>,
+  <ChakraProvider value={defaultSystem}>
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  </ChakraProvider>,
 );
